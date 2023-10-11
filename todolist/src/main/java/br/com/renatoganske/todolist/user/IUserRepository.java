c97520a3c1d93abea924e67ceb3d9aa6bd347b5d
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-10-11 00:28:53 -0300

    Aula 2

---
+++ todolist/src/main/java/br/com/renatoganske/todolist/user/IUserRepository.java
@@ -0,0 +1,12 @@
+package br.com.renatoganske.todolist.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+
+    UserModel findByUsername(String username);
+}
