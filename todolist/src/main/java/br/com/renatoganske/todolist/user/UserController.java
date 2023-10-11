8b7571624f5b7952d53230ce027421322b7c2f5b
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-10-10 23:48:03 -0300

    Initial commit

---
+++ todolist/src/main/java/br/com/renatoganske/todolist/user/UserController.java
@@ -0,0 +1,16 @@
+package br.com.renatoganske.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+}
