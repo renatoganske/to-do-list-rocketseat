3e7fdd7d46df7973c5c79ca78091e4bf3214ce0d
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-10-12 12:11:39 -0300

    Aula 3

---
+++ todolist/src/main/java/br/com/renatoganske/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,48 @@
+package br.com.renatoganske.todolist.filter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.renatoganske.todolist.user.IUserRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        var authorization = request.getHeader("Authorization");
+
+        var authEnconded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecoded = Base64.getDecoder().decode(authEnconded);
+
+        var authString = new String(authDecoded);
+
+        String[] credentials = authString.split(":");
+        var username = credentials[0];
+        var password = credentials[1];
+
+        var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+            response.sendError(401);
+        } else {
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if (passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            }
+        }
+    }
+}
